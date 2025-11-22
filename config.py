e0e2470af4466990f9c4219b093affb97eb6df66
Dorian <deneuchateldorian205@gmail.com>
2025-11-22 19:11:04 +0100

    Update config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+BASE_API_ROOT = "https://data.economie.gouv.fr"
+DATASET_PATH = "/api/explore/v2.1/catalog/datasets/prix-des-carburants-en-france-flux-instantane-v2/records"
+FULL_ENDPOINT = f"{BASE_API_ROOT}{DATASET_PATH}"
+DEFAULT_LIMIT = 20
+TIMEOUT = 10
